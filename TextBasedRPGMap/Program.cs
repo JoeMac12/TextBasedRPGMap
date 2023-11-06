fd763609505bee2e49bd13ea6d5b7fe1ec790ee5
Arman <ArmanLots2072@gmail.com>
2023-11-05 23:14:43 -0400

    Initial commit

---
+++ TextBasedRPGMap/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextBasedRPGMap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Text Based RPG Map");
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit the program");
+            Console.ReadKey(true);
+        }
+    }
+}
